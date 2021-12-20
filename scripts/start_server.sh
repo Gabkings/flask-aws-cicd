929d48488820992a2da612da925bdc6103296f2f
Gabriel Gitonga <gabworks51@gmail.com>
2021-12-20 14:43:46 +0300

    Adding dependences

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,7 +2,7 @@
 cd /home/ec2-user/app
 source environment/bin/activate
 sudo pip3 install -r requirements.txt
-sudo apt-get -y install supervisor
+# sudo apt-get -y install supervisor
 sudo unlink /var/run/supervisor.sock
 python src/app.py
 # supervisord -c supervisord.conf
