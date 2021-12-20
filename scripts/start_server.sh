df1c31ed9e3fd2a9d555c05ab271b4bd2877ff10
Gabriel Gitonga <gabworks51@gmail.com>
2021-12-20 14:59:30 +0300

    Adding dependences

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+sudo pip3 install virtualenv
 cd /home/ec2-user/app
+virtualenv environment
 source environment/bin/activate
+
 sudo pip3 install -r requirements.txt
 # sudo apt-get -y install supervisor
 # sudo unlink /var/run/supervisor.sock
